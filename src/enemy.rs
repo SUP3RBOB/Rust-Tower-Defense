953f289e0b088a19cf4f8b0961f42a183bd9b23a
Harris <moejoe494@gmail.com>
2024-01-18 22:19:50 -0500

    Initial Commit - Code is a little messy, just testing what bevy can do

---
+++ src/enemy.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Enemy {
+    speed: f32,
+    waypoint_id: usize,
+}
+
+impl Enemy {
+    pub fn new(spd: f32) -> Enemy {
+        return Enemy {
+            speed: spd,
+            waypoint_id: 0,
+        }
+    }
+}
