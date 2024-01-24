c253303ad2f0d2187eab99c2c7facac526c87e8b
Harris <moejoe494@gmail.com>
2024-01-23 21:31:08 -0500

    Towers can now rotate towards enemies

--- src/tower.rs
+++ src/tower.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
+use bevy::ui::debug::print_ui_layout_tree;
 
 use crate::enemy::Enemy;
+use crate::bullet::Bullet;
 
 #[derive(Component)]
 pub struct Tower {
@@ -9,8 +11,15 @@ pub struct Tower {
 }
 
 impl Tower {
-    pub fn get_closest_in_range(&self, player_pos: Vec3, transforms: &Query<&Transform, With<Enemy>>) -> Vec3 {
-        let mut closest = player_pos;
+    pub fn new(range_: f32) -> Tower {
+        return Tower {
+            range: range_,
+            direction: Vec3::ZERO,
+        }
+    }
+
+    pub fn get_closest_in_range(&self, player_pos: Vec3, transforms: &Query<&Transform, Without<Tower>>, out_closest: &mut Vec3) -> bool {
+        let mut closest = Vec3::ZERO;
 
         for transform in transforms.iter() {
             let prev_dist = Vec3::distance(player_pos, closest);
@@ -25,12 +34,13 @@ impl Tower {
             }
         }
 
-        return closest;
+        (*out_closest) = closest;
+        return (*out_closest) != Vec3::ZERO;
     }
 
     pub fn rotate_towards(&self, transform: &mut Transform, point: Vec3) {
         let difference = point - transform.translation;
-        let angle = f32::to_degrees(f32::atan2(difference.y, difference.x));
+        let angle = f32::atan2(difference.y, difference.x);
         transform.rotation = Quat::from_euler(EulerRot::XYZ, 0.0, 0.0, angle);
     }
 
@@ -54,12 +64,14 @@ pub fn place_tower(
             .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor)) {
 
             let spawn_position = Vec3::new(world_position.x, world_position.y, 0.0);
-            commands.spawn(SpriteBundle {
+            commands.spawn((SpriteBundle {
                 transform: Transform::from_translation(spawn_position),
                 texture: asset_server.load("sprites/tower.png"),
                 visibility: Visibility::Visible,
                 ..default()
-            });
+            },
+            Tower::new(300.0))
+            );
         }
     }
 }
@@ -67,12 +79,16 @@ pub fn place_tower(
 pub fn update_tower(
     mut commands: Commands,
     mut tower_query: Query<(&mut Tower, &mut Transform)>,
-    enemy_query: Query<&Transform, With<Enemy>>
+    enemy_query: Query<&Transform, Without<Tower>>
 ) {
     for (mut tower, mut transform) in tower_query.iter_mut() {
-        let closest = tower.get_closest_in_range(transform.translation, &enemy_query);
+        let mut closest = Vec3::ZERO;
+        let has_closest = tower.get_closest_in_range(transform.translation, &enemy_query, &mut closest);
+        //println!("{}, {}, {}", closest.x, closest.y, closest.z);
         tower.set_direction(Vec3::normalize(closest - transform.translation));
-        tower.rotate_towards(&mut transform, closest);
-        
+
+        if (has_closest) {
+            tower.rotate_towards(&mut transform, closest);
+        }
     }
 }
